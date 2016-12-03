527f8a03f4f2d9603bbf465c89689f62197bb719
RJ P <rj.pauloski@gmail.com>
2016-12-03 16:15:56 -0500

    Reordered library loads

--- setuptwitter.R
+++ setuptwitter.R
@@ -1,10 +1,3 @@
-# setup R to be abble to access live twitter data
-library(twitteR)
-consumer_key <- "your_consumer_key"
-consumer_secret <- "your_consumer_secret"
-access_token <- "your_access_token"
-access_secret <- "your_access_secret"
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 # setup environment to run twitter queries
 library(twitteR)
 library(dplyr)
@@ -14,3 +7,9 @@ library(RCurl)
 library(RJSONIO)
 library(stringr)
 library(e1071)
+# setup R to be abble to access live twitter data
+consumer_key <- "your_consumer_key"
+consumer_secret <- "your_consumer_secret"
+access_token <- "your_access_token"
+access_secret <- "your_access_secret"
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
